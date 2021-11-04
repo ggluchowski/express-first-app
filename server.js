bfe4e2fb9d92bc4b6ca85c16a1aa53814d307743
Grzegorz Głuchowski <ggrzegorz89@gmail.com>
2021-11-05 00:14:19 +0100

    Server to static folder

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); // stworzenie nowej aplikacji expressowej i przypisanie jej do stałej app
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `views/${name}`));
+  };
+  next();
+});
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+app.get('/contact', (req, res) => {
+  res.show('contact.html');
+});
+app.get('/info', (req, res) => {
+  res.show('info.html');
+});
+app.get('/history', (req, res) => {
+  res.show('history.html');
+});
+app.use((req,res) => {
+  res.status(404).send('404 not found...');
+})
+
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
